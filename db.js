eba9d6c7725bfc0b8ce701fadaa78fae9fe101b9
feras alnowiser <ferasdev@gmail.com>
2022-09-16 19:56:33 +0300

    add: connection method to connect the project with MongoDB and error notice function or succeed

--- db.js
+++ db.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 const mongooseURI = 'mongodb://localhost:27017/favFood';
 const db = mongoose.connection;
+
+mongoose.connect(mongooseURI, () => {
+    console.log('DATABASE IS CONNECTED ...');
+})
+
+db.on('error', err => {
+    console.log('Error occured during connecting MongoDB');
+});
+
+db.on('connected', () => {
+    console.log('MongoDB connected successfully');
+});
