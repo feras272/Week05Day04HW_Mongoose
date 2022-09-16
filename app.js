40e650eafa187a7e680b2a0d27af60fa50aa9225
feras alnowiser <ferasdev@gmail.com>
2022-09-16 19:45:15 +0300

    add: two main function for listening client requests and respond with the root page

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
+// database files
 const db = require("./db");
 const User = require("./models/users");
+
+// root page request
+app.get("/", (req, res) => {
+    res.json("<h1>Hello World</h1>");
+});
+
+// server start listening for client requests
+app.listen(port, () => {
+    console.log('SERVER IS WORKING ...');
+});
