e021e125d721b869540ab32897e69aa8d4f371bd
feras alnowiser <ferasdev@gmail.com>
2022-09-17 19:24:09 +0300

    add: get-users page with respond of all users in the database

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.get("/", (req, res) => {
     res.json("<h1>Hello World</h1>");
 });
 
+// show users page
+app.get('/get-users', (req, res) => {
+    User.find({}, (err, data) => {
+        if(err) {
+            console.log('Error, Can not find users');
+        } else {
+            res.json(data);
+        }
+    })
+})
+
 // server start listening for client requests
 app.listen(port, () => {
     console.log('SERVER IS WORKING ...');
--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const mongooseURI = 'mongodb://localhost:27017/favFood';
 const db = mongoose.connection;
 
-mongoose.connect(mongooseURI, () => {
-    console.log('DATABASE IS CONNECTED ...');
+mongoose.connect(mongooseURI);
+    /* , () => {
+    console.log('DATABASE IS ESTABLISHED ...');
 })
+*/
 
 db.on('error', (err) => {
     console.log('Error occured during connecting MongoDB');
