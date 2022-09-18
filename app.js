0fafab60331c4000fbc6ed83d0f35a023d57dc67
feras alnowiser <ferasdev@gmail.com>
2022-09-18 21:32:47 +0300

    add put request using update API to change old field (column) in a document

--- app.js
+++ app.js
@@ -26,15 +26,49 @@ app.get('/get-users', (req, res) => {
 
 // add user to 
 app.post("/post-users", (req, res) => {
-    User.create(req.body, function (err, small) {
+    User.create(req.body, function (err, newUser) {
       if (err) {
         console.log("ERROR => " + err);
         res.status(400).json("User Validation Failed");
       } else {
-        res.json("Success create new user");
+        res.json("Success create new user." + " Name : " +
+        req.body.fName + ", Favorite Food : " + 
+        req.body.favFood + ", And the Age : " + 
+        req.body.age + " year old");
       }
     });
   });  
+
+app.put('/put-users/fName/:oldName', (req, res) => {
+    User.updateOne(
+        { fName: req.params.oldName },
+        { fName: req.body.newName },
+        (err, updatedObj) => {
+          if (err) {
+            console.log("ERROR => " + err);
+            res.status(500).json("There is a Problem with the Database");
+          } else {
+            console.log(updatedObj);
+            if (updatedObj.matchedCount === 0) {
+              console.log(updatedObj);
+              res.status(404).json("Cannot Update User [" + req.params.oldName + "] Not Found");
+            } else {
+              console.log(
+                "User Updated from " +
+                  req.params.oldName +
+                  " To User name " +
+                  req.body.newName
+              );
+              res
+                .status(200)
+                .json(
+                  "Success Update " + req.params.oldName + " To " + req.body.newName
+                );
+            }
+          }
+        }
+      );
+});
 
 // server start listening for client requests
 app.listen(port, () => {
